5e9d880759fd3b63d49f80387c1a7f3d0168166d
Thomastucieux <thomas-un@hotmail.fr>
2017-05-19 15:51:07 +0200

    Ajout projet

---
+++ Projet/Projet/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Projet
+{
+    partial class frmAppli
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmAppli
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 412);
+            this.Name = "frmAppli";
+            this.Text = "Vif Argent";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
