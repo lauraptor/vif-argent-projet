724b49e07ceadb485291bd9bd35d040eafaf7475
Thomastucieux <thomas-un@hotmail.fr>
2017-05-29 16:08:22 +0200

    Add files via upload

--- Projet/Projet/Form1.cs
+++ Projet/Projet/Form1.cs
@@ -13,14 +13,83 @@ namespace Projet
 {
     public partial class frmAppli : Form
     {
+
+        OleDbConnection connec = new OleDbConnection();
         public frmAppli()
         {
             InitializeComponent();
         }
 
+    
+        
+        
+
         private void frmAppli_Load(object sender, EventArgs e)
         {
 
+            try
+            {
+                connec.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\\pie\tunterfinger\A21\Projet\Git\budget1.mdb";
+                connec.Open();
+                OleDbCommand command = new OleDbCommand();
+                command.Connection = connec;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "select libType from TypeTransaction";
+                OleDbDataReader dr = command.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        cboType.Items.Add(dr.GetString(0));
+
+                    }
+
+                    this.cboType.SelectedIndex = 0;
+                    cboType.DropDownStyle = ComboBoxStyle.DropDownList;
+                }
+                else
+                {
+                    MessageBox.Show("Pas de catégorie trouvées");
+                }
+            }
+
+
+            catch(Exception error)
+            {
+                MessageBox.Show(error.GetType().ToString());
+            }
+
+            finally
+            {
+                if(connec.State == ConnectionState.Open)
+                {
+                    connec.Close();
+                }
+                    
+            }
+        }
+
+
+        private void btnAjoutType_Click(object sender, EventArgs e)
+        {
+            //Affichage du formulaire de création d'un libellé
+            Libelle_Type frm = new Libelle_Type();
+
+            frm.Show();
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            //Vérification que le formulaire soit rempli avant la création d'une transaction
+            if (txtDescription.Text == String.Empty || txtMontant.Text == String.Empty || (rdbPercu.Checked && rdbRecette.Checked) || (!rdbRecette.Checked && !rdbPercu.Checked))
+            {
+                MessageBox.Show("Veuillez remplir tout les champs");
+            }
+            else
+            {
+
+            }
         }
     }
 }
+
---
+++ Projet/Projet/Libelle_Type.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet
+{
+    public partial class Libelle_Type : Form
+    {
+        public Libelle_Type()
+        {
+            InitializeComponent();
+        }
+
+        private void Libelle_Type_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAjoutType_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+        }
+    }
+}
